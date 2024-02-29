02bcf0f3adb059cfaacf6fb08148849c0475c550
antonyevchik <av.yevchik@gmail.com>
2024-02-29 19:34:11 +0200

    add title to search query

--- app/Http/Controllers/PostsSearchController.php
+++ app/Http/Controllers/PostsSearchController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\SearchPostRequest;
 use App\Http\Resources\PostResource;
-use App\Models\Post;
+use App\Models\PostTranslation;
 
 class PostsSearchController extends Controller
 {
     public function search(SearchPostRequest $request)
     {
-        return PostResource::collection(Post::get())->getIterator();
+        return PostResource::collection(
+            PostTranslation::when($request->filled('title'), fn($query) =>
+                $query->where('title', 'like', '%' . $request->validated('query') . '%')
+            )
+            ->latest()->get())
+//            ->getIterator()
+            ;
     }
 }
--- app/Http/Requests/SearchPostRequest.php
+++ app/Http/Requests/SearchPostRequest.php
@@ -22,7 +22,7 @@ class SearchPostRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => 'sometimes|string'
         ];
     }
 }
