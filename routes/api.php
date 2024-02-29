9c539b196398a0e24237c79028ee67e21ebf93c1
antonyevchik <av.yevchik@gmail.com>
2024-02-29 23:12:00 +0200

    resolving authentication

--- routes/api.php
+++ routes/api.php
@@ -20,10 +20,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::group(['prefix' => 'posts'], function () {
         Route::get('/', [PostsController::class, 'index'])->name('posts.index');
-        Route::get('/find_by_id/{post}', [PostsController::class, 'findById'])->name('posts.find-by-id');
+        Route::get('/find-by-id/{post}', [PostsController::class, 'findById'])->name('posts.find-by-id');
         Route::post('/', [PostsController::class, 'store'])->name('posts.store');
         Route::put('/{post}', [PostsController::class, 'update'])->name('posts.update');
         Route::delete('/{post}', [PostsController::class, 'destroy'])->name('posts.destroy');
