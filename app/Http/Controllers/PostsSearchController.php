03db09fd3afdd6d6f687a734d6cba5649a2d6dbb
antonyevchik <av.yevchik@gmail.com>
2024-02-29 19:08:53 +0200

    remove manual iterator

--- app/Http/Controllers/PostsSearchController.php
+++ app/Http/Controllers/PostsSearchController.php
@@ -3,27 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SearchPostRequest;
+use App\Http\Resources\PostResource;
 use App\Models\Post;
-use App\Services\PostAggregate;
-use Illuminate\Http\Request;
 
 class PostsSearchController extends Controller
 {
     public function search(SearchPostRequest $request)
     {
-        $posts = Post::get()->toArray();
-
-        $aggregate = new PostAggregate($posts);
-        $iterator = $aggregate->createIterator();
-
-        dd($iterator->first());
-
-        $results = [];
-
-        foreach ($iterator as $post) {
-            $results[] = $post;
-        }
-
-        return $results;
+        return PostResource::collection(Post::get())->getIterator();
     }
 }
